b9ed83d404788f08c4e6008787f9aff50f4a89c6
dev_krissh-at-629937844728 <krishnamitws2000@gmail.com>
2024-04-03 01:14:30 +0530

    Update

--- onlinenews/onlinenews/settings.py
+++ onlinenews/onlinenews/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*t!p447b)rhx@4q^_hw982cnmobnfw*l-ts*2=%jyss^a)4kbt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'onlinenews.urls'
@@ -118,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+#Add this in your settings.py file:
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystaticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
